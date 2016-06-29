2462b692559d6a411db33764571761a7e7677430
Ubuntu <ubuntu@ip-172-30-0-94.ec2.internal>
2016-06-29 03:48:26 +0000

    working now

---
+++ setEnv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export DOCKER_IP=`ip -o -4 addr list eth0 | grep global | awk '{print $4}' | cut -d/ -f1`
+
--- setHostIP.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-export HOST_IP=`ip -o -4 addr list eth0 | grep global | awk '{print $4}' | cut -d/ -f1`
